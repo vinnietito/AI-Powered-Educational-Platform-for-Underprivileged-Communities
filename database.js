d1fd450bc4abba62437afe7cc8bd2df7e6e376b6
vinnietito <vincentnjuguna69@gmail.com>
2024-11-23 09:45:12 +0300

    First Commits

---
+++ database.js
@@ -0,0 +1,18 @@
+const mysql = require('mysql2');
+
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root', // Your MySQL username
+    password: 'password', // Your MySQL password
+    database: 'digital_edu_platform'
+});
+
+db.connect((err) => {
+    if (err) {
+        console.error('Database connection failed:', err.message);
+    } else {
+        console.log('Connected to MySQL database.');
+    }
+});
+
+module.exports = db;
