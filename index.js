b67ae600d6aeb408a8b83ebf602c639ac26eea43
vinnietito <vincentnjuguna69@gmail.com>
2024-11-23 09:56:45 +0300

    Commits

--- index.js
+++ index.js
@@ -6,3 +6,12 @@ const courseRoutes = require('./routes/courses');
 const progressRoutes = require('./routes/progress');
 
 const app = express();
+
+//Middleware
+app.use(cors());
+app.use(bodyParser.json());
+
+//Routes
+app.use('/api/auth', authRoutes);
+app.use('/api/courses', courseRoutes);
+app.use('api/progress', progressRoutes);
