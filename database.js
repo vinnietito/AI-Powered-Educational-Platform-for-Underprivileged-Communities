246e24e3f422e169e35534299c7b460282fbe11f
vinnietito <vincentnjuguna69@gmail.com>
2024-11-23 10:18:11 +0300

    Commits

--- database.js
+++ database.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql2');
 
 const db = mysql.createConnection({
-    host: 'localhost',
-    user: 'root', // Your MySQL username
-    password: 'V@38080135k', // Your MySQL password
-    database: 'digital_edu_platform'
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
 });
 
 db.connect((err) => {
